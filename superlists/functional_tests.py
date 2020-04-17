9645a11eae519c31d232cf1dd33fb73095f49c1d
Luiz Eduardo Barros <luizbarros6@ufrn.edu.br>
2020-04-17 13:52:13 -0300

    Functional test now checks we can input a to-do item

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,14 +1,15 @@
 from selenium import webdriver
 import unittest
 
-class New VisitorTest(unittest.TestCase):
+class NewVisitorTest(unittest.TestCase):
     def setUp(self):
-        browser = webdriver.Firefox()
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
         # Satisfied, she goes back to sleep
-        browser.quit()
-        
+        self.browser.quit()
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
@@ -16,19 +17,34 @@ class New VisitorTest(unittest.TestCase):
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!)
-        
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
         # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # She types "Buy peackock feathers" into a text box  (Edith's hobby
         # is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peackock feathers" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
+        )
 
         # There is still a text box inviting her to add another item. She
         # enter "Use peacock feathers to make a fly" (Edith is very methodical)
-
+        self.fail('Finish the test!')
         # The page updates again, and now shows both item on her list
 
         # Edith wonders whether the site will remember her list. Then she sees
@@ -39,4 +55,3 @@ class New VisitorTest(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
-
