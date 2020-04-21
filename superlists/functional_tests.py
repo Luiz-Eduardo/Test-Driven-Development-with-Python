7b0ae4b7c5cb8aa863d493e3f41e43f2af0fb520
Luiz Eduardo Barros <luizbarros6@ufrn.edu.br>
2020-04-21 20:13:01 -0300

    Refactoring functional tests

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -15,6 +15,12 @@ class NewVisitorTest(unittest.TestCase):
         # Satisfied, she goes back to sleep
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
@@ -44,9 +50,7 @@ class NewVisitorTest(unittest.TestCase):
             expected_conditions.text_to_be_present_in_element(
                 (By.ID, 'id_list_table'), 'Buy peacock feathers'))
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
         # enter "Use peacock feathers to make a fly" (Edith is very methodical)
@@ -58,9 +62,8 @@ class NewVisitorTest(unittest.TestCase):
             expected_conditions.text_to_be_present_in_element(
                 (By.ID, 'id_list_table'), 'Use peacock feathers to make a fly'))
         # The page updates again, and now shows both item on her list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
